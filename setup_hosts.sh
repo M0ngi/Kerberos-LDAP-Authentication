507933894eb78b3b1cfdaee773844bffb1e8a403
M0ngi <saidanemongi@gmail.com>
2024-01-18 10:19:44 +0100

    Update

--- setup_hosts.sh
+++ setup_hosts.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 echo "0.0.0.0         insat.tn" >> /etc/hosts
+echo "0.0.0.0         web.insat.tn" >> /etc/hosts
+echo "0.0.0.0         ldap.insat.tn" >> /etc/hosts
+echo "0.0.0.0         openvpn.insat.tn" >> /etc/hosts
+echo "0.0.0.0         ssh.insat.tn" >> /etc/hosts
 echo "172.50.0.2      dns.insat.tn" >> /etc/hosts
